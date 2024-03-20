bdc7425eae64bda9676b4a33c9c247b4be7f3ce9
Juanita Munar Giraldo <juanita.munar0.0@gmail.com>
2024-03-20 15:17:18 -0500

    variables

--- intropyhton.py
+++ intropyhton.py
@@ -1,2 +1,23 @@
 #comentario de una linea
 #todo lo que va despues es ignorado por el interprete de python
+
+#Variables: Espacio de memoria, con nombre, donde guardo valores
+#los nombres de variables deben ser cortos, descriptivos, NO TENER ESPACIOS
+#EN BLANCO ni caracteres especiales, ni numeros
+
+#Descriptivo significa que representa la categoria del dato que quiero guardar
+#En python las variables pueden contener cualquier dato de tipo primitivo
+#D.P: numeros enteros o reales, cadena de caracteres (string), boleanos (True, FALSE), caracteres
+
+variable= 1 
+
+variable= 'Cadena de texto'
+
+variable= True
+
+variable= 'a'
+
+variable= 3.1415926535
+
+# Para asignar un valor a la variable se usa el operador = 
+
