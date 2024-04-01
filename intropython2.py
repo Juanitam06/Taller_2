93c49b31777829f69827bad35573c88a1e95701e
Juanita Munar Giraldo <juanita.munar0.0@gmail.com>
2024-04-01 14:54:25 -0500

    ciclos

---
+++ intropython2.py
@@ -0,0 +1,15 @@
+#while
+#while <condicion verdadera>
+#     cuerpo del ciclo
+#Condiciones son: expresiones booleanas (or,and) y operaciones de comparacion
+#cicclos controlados por un controlador entero
+
+i=0
+
+while i<10:
+    print("ciclo")
+    #importante modificar el valor del contador
+    i=i+1  i+=1
+     
+while 1==1
+    print("ciclo")
